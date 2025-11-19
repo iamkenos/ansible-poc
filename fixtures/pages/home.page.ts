d11a25958befaa14199e8754b5e50b74c4ecdcbd
iamkenos <alexis.matunog@gmail.com>
2025-11-19 16:30:31 +1100

    feat: tosca bits

--- fixtures/pages/home.page.ts
+++
@@ -1,9 +0,0 @@
-import { PageObject } from "@iamkenos/kyoko";
-import { Header } from "~/fixtures/components/header.component";
-
-export class HomePage extends PageObject {
-  url = "/";
-  title = "Services Australia";
-
-  header = () => this.page.component(Header);
-}
